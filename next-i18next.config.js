0e7996577a340e55596aaa3bf53d0610758ad8fb
alinkaLife <76113800+alinkaLife@users.noreply.github.com>
2023-10-09 09:14:14 -0400

    added localePath

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
     i18n: {
         locales: ['en', 'ru', 'ua', 'kz', 'uz'],
         defaultLocale: 'en',
         localeDetection: false,
+        localePath: path.resolve('./public/locales')
     },
 }
