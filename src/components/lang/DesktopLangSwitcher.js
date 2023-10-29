3561112370bdeeb731021bfc0324a732c0b29faf
alinkaLife <76113800+alinkaLife@users.noreply.github.com>
2023-10-29 15:31:28 -0400

    change localisation

--- src/components/lang/DesktopLangSwitcher.js
+++ src/components/lang/DesktopLangSwitcher.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import RuIcon from '@/assets/icons/geo/1.png'
 import EngIcon from '@/assets/icons/en_icon.svg'
 import Image from 'next/image'
+import { ClickAwayListener } from '@mui/base/ClickAwayListener'
 
 const DesktopLangSwitcher = () => {
     const router = useRouter()
@@ -19,49 +20,55 @@ const DesktopLangSwitcher = () => {
     const isRuLang = router.locale === 'ru'
 
     return (
-        <div className={styles.switcher__wrapper}>
-            <div onClick={handleToggle} className={styles.switcher}>
-                <Image
-                    width={24}
-                    height={24}
-                    src={!isRuLang ? EngIcon : RuIcon}
-                    priority={true}
-                    alt="arrow"
-                />
-            </div>
+        <ClickAwayListener
+            onClickAway={() => {
+                isOpen && setIsOpen(false)
+            }}
+        >
+            <div className={styles.switcher__wrapper}>
+                <div onClick={handleToggle} className={styles.switcher}>
+                    <Image
+                        width={24}
+                        height={24}
+                        src={!isRuLang ? EngIcon : RuIcon}
+                        priority={true}
+                        alt="arrow"
+                    />
+                </div>
 
-            <ul
-                className={[
-                    styles.list,
-                    !isOpen ? styles.list__isHidden : undefined,
-                ].join(' ')}
-            >
-                <li
-                    onClick={() => {
-                        handleChange(!isRuLang ? 'ru' : 'en')
-                    }}
+                <ul
                     className={[
-                        styles.list__item,
-                        isRuLang ? styles.isSelected : undefined,
+                        styles.list,
+                        !isOpen ? styles.list__isHidden : undefined,
                     ].join(' ')}
                 >
-                    <Image width={14} height={14} src={RuIcon} alt="ru" />
-                    <span>RU</span>
-                </li>
-                <li
-                    onClick={() => {
-                        handleChange(!isRuLang ? 'ru' : 'en')
-                    }}
-                    className={[
-                        styles.list__item,
-                        !isRuLang ? styles.isSelected : undefined,
-                    ].join(' ')}
-                >
-                    <Image width={14} height={14} src={EngIcon} alt="en" />
-                    <span>EN</span>
-                </li>
-            </ul>
-        </div>
+                    <li
+                        onClick={() => {
+                            handleChange(!isRuLang ? 'ru' : 'en')
+                        }}
+                        className={[
+                            styles.list__item,
+                            isRuLang ? styles.isSelected : undefined,
+                        ].join(' ')}
+                    >
+                        <Image width={14} height={14} src={RuIcon} alt="ru" />
+                        <span>RU</span>
+                    </li>
+                    <li
+                        onClick={() => {
+                            handleChange(!isRuLang ? 'ru' : 'en')
+                        }}
+                        className={[
+                            styles.list__item,
+                            !isRuLang ? styles.isSelected : undefined,
+                        ].join(' ')}
+                    >
+                        <Image width={14} height={14} src={EngIcon} alt="en" />
+                        <span>EN</span>
+                    </li>
+                </ul>
+            </div>
+        </ClickAwayListener>
     )
 }
 
